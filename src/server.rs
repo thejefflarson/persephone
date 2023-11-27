71e5dd4be72c48ebe3b5074b2fa0febb19bb6c03
Jeff Larson <thejefflarson@gmail.com>
2023-11-27 08:21:57 -0800

    add sqlx build script

--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,32 @@
+use anyhow::Result;
+use async_graphql::{http::GraphiQLSource, EmptyMutation, Object, Schema, Subscription};
+use axum::response::{Html, IntoResponse};
 
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn ask(&self) -> &'static str {
+        "heyo"
+    }
+}
+
+struct SubscriptionRoot;
+
+#[Subscription]
+impl SubscriptionRoot {
+    async fn ask(&self) -> &'static str {
+        "heyo"
+    }
+}
+
+type AssistantSchema = Schema<QueryRoot, EmptyMutation, SubscriptionRoot>;
+
+async fn graphiql() -> impl IntoResponse {
+    Html(GraphiQLSource::build().finish())
+}
+
+async fn start() -> Result<()> {
+    let schema = AssistantSchema::build(QueryRoot, EmptyMutation, SubscriptionRoot).finish();
+    Ok(())
+}
