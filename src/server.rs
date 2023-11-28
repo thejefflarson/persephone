0a5c206cbaf0294027722ee018d160bcc6fabf7e
Jeff Larson <thejefflarson@gmail.com>
2023-11-27 21:47:38 -0800

    building

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
-use async_graphql::{http::GraphiQLSource, EmptyMutation, Object, Schema, Subscription};
+use async_graphql::{
+    futures_util::{stream, Stream},
+    http::GraphiQLSource,
+    EmptyMutation, Object, Schema, Subscription,
+};
 use axum::response::{Html, IntoResponse};
 
 struct QueryRoot;
@@ -15,8 +19,8 @@ struct SubscriptionRoot;
 
 #[Subscription]
 impl SubscriptionRoot {
-    async fn ask(&self) -> &'static str {
-        "heyo"
+    async fn ask(&self) -> impl Stream<Item = &'static str> {
+        stream::iter(vec!["heyo"])
     }
 }
 
