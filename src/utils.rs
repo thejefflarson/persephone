e216521db23f854c71b52d9a457cb7e4622fbecc
Jeff Larson <thejefflarson@gmail.com>
2024-01-20 15:16:21 -0800

    fingers crossed

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use candle_core::{
+    utils::{cuda_is_available, metal_is_available},
+    Device,
+};
+
+pub fn device() -> Result<Device> {
+    if metal_is_available() {
+        Ok(Device::new_metal(0)?)
+    } else if cuda_is_available() {
+        Ok(Device::new_cuda(0)?)
+    } else {
+        Ok(Device::Cpu)
+    }
+}
