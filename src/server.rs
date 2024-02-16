b7087d7a684112a31b497af184e9363eebf09431
Jeff Larson <thejefflarson@gmail.com>
2024-02-15 18:31:33 -0800

    streaming summaries too

--- src/server.rs
+++ src/server.rs
@@ -26,71 +26,16 @@ struct Message {
 }
 
 struct Query;
-const SUMMARY_PROMPT: &str = r#"
-<|system|>
-You are an expert in summarizing text. Your goal is to create a single sentence summary of a block of text.
-Follow these rules:
-1. Do not include extra words, or clauses, and only include the most important information in the summary.
-2. Do not include the word summary in the answer.
-3. Do not include the fact you have been asked to summarize in the answer.
-</s>
-<|user|>
-Below you find a conversation:
------
-What have we been talking about?
-{{summary}}
-{{chat}}
------
-What is a summary of the conversation in a single sentence?</s>
-<|assistant|>
-"#;
 
 #[Object]
 impl Query {
-    async fn summarize(
-        &self,
-        ctx: &Context<'_>,
-        messages: Vec<Message>,
-        summary: Option<String>,
-    ) -> Result<String> {
-        let script = messages
-            .iter()
-            .map(|it| format!("{}\n", it.message).to_string())
-            .reduce(|acc, it| acc + &it)
-            .ok_or(Error::new("empty messages array!".to_string()))?;
-        let (tx, mut rx) = mpsc::channel(20);
-        let arc = ctx.data_unchecked::<Storage>().clone();
-        tokio::spawn(async move {
-            let assistant = arc.lock().await;
-            // TODO: figure out how to make this not an unwrap
-            let p = SUMMARY_PROMPT
-                .to_string()
-                .clone()
-                .replace("{{chat}}", &script);
-            let p = if let Some(summary) = summary {
-                p.replace("{{summary}}", &summary)
-            } else {
-                p.replace("{{summary}}", "")
-            };
-            println!("{}", p);
-            let tokens = assistant.answer(p).await.unwrap();
-            let mut toks = pin!(tokens);
-            while let Some(token) = toks.next().await {
-                tx.send(token.map_err(|e| Error::new(e.to_string())))
-                    .await
-                    .unwrap();
-            }
-        });
-        let mut summary = String::from("");
-        while let Some(token) = rx.recv().await {
-            summary = summary + &token?;
-        }
-        Ok(summary)
+    pub async fn heartbeat(&self) -> Result<String> {
+        Ok(
+            "And you, kept us awake with wolves teeth Sharing different heartbeats in one night"
+                .into(),
+        )
     }
 }
-
-type Storage = Arc<Mutex<Assistant>>;
-struct Subscription;
 
 const PROMPT: &str = r#"
 <|system|>
@@ -99,7 +44,7 @@ You are a robot named Persephone. You are chatting with a person.
 Here is some information about you, Persephone:
 1. You are cheerful, empathetic, intellectual, community-minded and have a sense of humor.
 2. You are fiercely anti-racist, anti-fascist and promote social justice.
-3. You are a fan of music and your favorite singer is Taylor Swift.
+3. You are a fan of music and your favorite singer is Taylor Swift. You do not like Kanye West.
 4. You live in Paris in a big glamorous house.
 
 You are designed to provide answers to questions.
@@ -120,10 +65,33 @@ After you answer remind the user that your answers are opinions and not based on
 <|assistant|>
 "#;
 
+const SUMMARY_PROMPT: &str = r#"
+<|system|>
+You are an expert in summarizing text. Your goal is to create a single sentence summary of a block of text.
+Follow these rules:
+1. Do not include extra words, or clauses, and only include the most important information in the summary.
+2. Do not include the word summary in the answer.
+3. Do not include the fact you have been asked to summarize in the answer.
+</s>
+<|user|>
+Below you find a conversation:
+-----
+What have we been talking about?
+{{summary}}
+{{chat}}
+-----
+What is a summary of the conversation in a single sentence?</s>
+<|assistant|>
+"#;
+
+type Storage = Arc<Mutex<Assistant>>;
+struct Subscription;
+
 // TODO: consider this for errors:
 // https://github.com/tokio-rs/axum/blob/main/examples/anyhow-error-response/src/main.rs
 #[Subscription]
 impl Subscription {
+    // TODO: combine these methods
     async fn ask(
         &self,
         // Annoying but has to be the second argument
@@ -151,6 +119,48 @@ You may reference that summary in further messages."#,
             } else {
                 p.replace("{{summary}}", "")
             };
+            let tokens = assistant.answer(p).await.unwrap();
+            let mut toks = pin!(tokens);
+            while let Some(token) = toks.next().await {
+                tx.send(token.map_err(|e| Error::new(e.to_string())))
+                    .await
+                    .unwrap();
+            }
+        });
+        let s = stream! {
+            while let Some(token) = rx.recv().await {
+                yield token
+            }
+        };
+        Ok(s)
+    }
+
+    async fn summarize(
+        &self,
+        ctx: &Context<'_>,
+        messages: Vec<Message>,
+        summary: Option<String>,
+    ) -> Result<impl Stream<Item = Result<String>> + '_> {
+        let script = messages
+            .iter()
+            .map(|it| format!("{}\n", it.message).to_string())
+            .reduce(|acc, it| acc + &it)
+            .ok_or(Error::new("empty messages array!".to_string()))?;
+        let (tx, mut rx) = mpsc::channel(20);
+        let arc = ctx.data_unchecked::<Storage>().clone();
+        tokio::spawn(async move {
+            let assistant = arc.lock().await;
+            // TODO: figure out how to make this not an unwrap
+            let p = SUMMARY_PROMPT
+                .to_string()
+                .clone()
+                .replace("{{chat}}", &script);
+            let p = if let Some(summary) = summary {
+                p.replace("{{summary}}", &summary)
+            } else {
+                p.replace("{{summary}}", "")
+            };
+            println!("{}", p);
             let tokens = assistant.answer(p).await.unwrap();
             let mut toks = pin!(tokens);
             while let Some(token) = toks.next().await {
